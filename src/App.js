f4603a18946cf467cac1ada62d63b26c1ff2f504
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-09-08 15:44:13 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
         >
-          Learn React
-        </a>
+          <Link to="/todos">Todos</Link> |{" "}
+          <Link to="/newsletter">Newsletter</Link>
+        </nav>
       </header>
     </div>
   );
