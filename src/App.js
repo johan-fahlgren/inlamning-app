7328acdf152cce7280114bbb494062615f549078
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-09-09 16:08:49 +0200

    in progress - modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Modal from "./components/Modal";
 
 function App() {
+  const name = "Johan";
+  const email = "johanfahlgren89@gmail.com";
+
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
+        {/* <Navbar /> */}
+        <Modal mName={name} mEmail={email} />
       </header>
     </div>
   );
